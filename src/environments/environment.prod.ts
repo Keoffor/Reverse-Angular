a0c97df767923bdef5ef11c836a8b1d9348bc2ed
keoffor <kenobago@gmail.com>
2022-03-29 17:48:28 -0500

    changes made

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  GATEWAY_URI: "http://35.223.254.189:8084/",
-  USER_URI: "http://34.121.99.46:8081/",
-  POST_URI: "http://35.192.86.61:8000/",
-  LIST_URI: "http://34.121.99.46:8081/",
-  COMMENT_URI: "http://35.192.86.61:8000/"
+  GATEWAY_URI: "http://34.136.116.167:8084/",
+  USER_URI: "http://34.122.38.138:8081/",
+  POST_URI: "http://34.122.57.215:8000/",
+  LIST_URI: "http://34.122.38.138:8081/",
+  COMMENT_URI: "http://107.178.213.197:8000/"
 };
